3e4f7bd1307e01a4a364595910c3c3380a394fd1
daishir0 <daishiro.hirashima@gmail.com>
2017-09-05 20:51:09 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 # This function reads your DATABASE_URL config var and returns a connection
-# string suitable for pg_connect. Put this in your app.
+# string suitable for pg_connect. Put this in your app. test
 function pg_connection_string_from_database_url() {
   extract(parse_url($_ENV["DATABASE_URL"]));
   return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
