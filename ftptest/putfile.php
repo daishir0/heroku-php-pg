da957fcc817cb3701db5ecf34b357a62b31e7901
daishir0 <daishiro.hirashima@gmail.com>
2017-10-17 17:12:07 +0900

    mod putfile.php for improve

--- ftptest/putfile.php
+++ ftptest/putfile.php
@@ -7,16 +7,20 @@ define("FTP_ACCOUNT", getenv("FTP_ACCOUNT"));
 define("FTP_PASS", getenv("FTP_PASS"));
 
 // 接続を確立する
-$conn_id = ftp_connect(FTP_SERVER);
+$conn_id = ftp_connect(FTP_SERVER) or die("Couldn't connect to ") . FTP_SERVER;
 
 // ユーザー名とパスワードでログインする
 $login_result = ftp_login($conn_id, FTP_ACCOUNT, FTP_PASS);
+
+//パッシブモードをオンにする
+ftp_pasv($conn_id, true);
 
 // ファイルをアップロードする
 if (ftp_put($conn_id, $remote_file, $input_file, FTP_BINARY)) {
  echo "successfully uploaded $input_file\n";
 } else {
- echo "There was a problem while uploading $file\n";
+ echo "There was a problem while uploading $input_file\n";
+ echo FTP_SERVER . "\n";
 }
 
 // 接続を閉じる
