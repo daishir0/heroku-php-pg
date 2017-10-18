20d48a0dd51b8542a54962d1cdedc7b478ebab18
daishir0 <daishiro.hirashima@gmail.com>
2017-10-18 14:14:26 +0900

    move filedl.php, and add jquery.js

--- fileupload/index.php
+++ fileupload/index.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script type="text/javascript" src="/js/jquery-3.2.1.slim.min.js"></script>
 <script type="text/javascript" src="fileupload.js"></script>
 </head>
 <body>
